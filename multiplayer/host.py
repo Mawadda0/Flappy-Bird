29c2e85300e136b5ce3eaa3a3a0314df49acb0b8
Moza <moazw9969@gmail.com>
2025-12-20 20:09:03 +0200

    Added bird and basic config

--- multiplayer/host.py
+++ multiplayer/host.py
@@ -46,3 +46,65 @@ def send_map_coordinates(client_socket):
     # Serialize the list and send it
     data = pickle.dumps(coordinates)
     client_socket.send(data)
+
+
+
+#-------------------------------------#-------------------------------------#----------------------------
+# 1. SETUP & CONFIG
+#-------------------------------------#-------------------------------------#----------------------------
+pygame.init()
+
+screen_info = pygame.display.Info()
+WIDTH, HEIGHT = 800, 600
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Bird")
+
+clock = pygame.time.Clock()
+FPS = 60
+
+#-------------------------------------#-------------------------------------#----------------------------
+# bird handler
+#-------------------------------------#-------------------------------------#----------------------------
+BIRD_SIZE = 80
+try:
+    bird_image = pygame.image.load("./version_two/bird2.png").convert_alpha()
+    bird_image = pygame.transform.scale(bird_image, (BIRD_SIZE, BIRD_SIZE))
+except:
+    bird_image = pygame.Surface((BIRD_SIZE, BIRD_SIZE))
+    bird_image.fill((255, 255, 0))
+
+max_down_speed = 100
+gravity = 0.4
+jump_strength = -5
+
+class Bird():
+    def __init__(self):
+        self.x = WIDTH / 3
+        self.y = HEIGHT / 2
+        self.speed_y = 0
+
+bird = Bird()
+
+def jump_bird(bird):
+    bird.speed_y = jump_strength
+
+def update_bird(bird):
+    bird.speed_y += gravity
+    if bird.speed_y > max_down_speed:
+        bird.speed_y = max_down_speed
+    
+    bird.y += bird.speed_y
+
+    if bird.y < 0:
+        bird.y = 0            
+        bird.speed_y = 0      
+
+    angle = -bird.speed_y * 2
+    rotated_bird = pygame.transform.rotate(bird_image, angle)
+    bird_rect = rotated_bird.get_rect(center = (bird.x + BIRD_SIZE / 2, bird.y + BIRD_SIZE / 2))
+    screen.blit(rotated_bird, bird_rect.topleft)
+
+def get_bird_hitbox(bird):
+    return pygame.Rect(bird.x + 25, bird.y + 25, BIRD_SIZE - 50, BIRD_SIZE - 50)
+
+#-------------------------------------#-------------------------------------#----------------------------
