704689778fcef01b7cdae7999e7daa91da07ee3f
Moza <moazw9969@gmail.com>
2025-12-20 18:53:05 +0200

    Added cordinate list generator

--- multiplayer/client.py
+++ multiplayer/client.py
@@ -2,6 +2,7 @@ import pygame
 import random
 import socket
 import threading
+import pickle
 
 #ports init
 BROADCAST_PORT = 50000
@@ -12,6 +13,35 @@ seed = None
 
 #search for host message that is broadcasted
 def find_host():
+    pass
 
 #waiting for the starting message from host
 def wait_for_start():
+    pass
+
+def generate_client_pipes(client_socket, pipes_list, Pipe_Class, GAME_HEIGHT):
+    # Receive the data (buffer size large enough for the list)
+    data = client_socket.recv(4096 * 8)
+    coordinates = pickle.loads(data)
+    
+    # Constants for spacing (Calculated from main.py: 1500ms timer * 60fps * 2 speed = 180px)
+    start_x = 800
+    spacing = 180 
+    TOP = True
+    BOTTOM = False
+    
+    for i, y in enumerate(coordinates):
+        current_x = start_x + (i * spacing)
+        open_space = GAME_HEIGHT / 3
+        
+        # Create Top Pipe using the received Y
+        top_pipe = Pipe_Class(TOP)
+        top_pipe.y = y
+        top_pipe.x = current_x  # Position it correctly in the timeline
+        pipes_list.append(top_pipe)
+        
+        # Create Bottom Pipe based on Top Pipe
+        bottom_pipe = Pipe_Class(BOTTOM)
+        bottom_pipe.y = top_pipe.y + top_pipe.height + open_space
+        bottom_pipe.x = current_x
+        pipes_list.append(bottom_pipe)
--- multiplayer/host.py
+++ multiplayer/host.py
@@ -3,6 +3,7 @@ import random
 import socket
 import threading
 import time
+import pickle
 
 #seed and ports configs
 BROADCAST_PORT = 50000
@@ -13,7 +14,25 @@ clients = set()
 seed = random.randint(0, 999999)
 #the host init for broadcasting message
 def broadcast_host():
-
+  pass
 #waiting for the messages from the client
 def udp_listener():
-   
+      pass
+
+ 
+
+def send_map_coordinates(client_socket):
+    # Constants from main.py used for generation
+    pipe_y = 0
+    pipe_height = 512
+    coordinates = []
+    
+    # Generate 100 pipes (enough for a long game)
+    for _ in range(100):
+        # The exact math from main.py:
+        random_pipe_y = pipe_y - pipe_height/4 - random.random()*(pipe_height/2)
+        coordinates.append(random_pipe_y)
+    
+    # Serialize the list and send it
+    data = pickle.dumps(coordinates)
+    client_socket.send(data)
